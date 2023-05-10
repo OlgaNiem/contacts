ded546e7c1a8525566116e0e542b2286d611fc35
OlgaNiem <102143760+OlgaNiem@users.noreply.github.com>
2023-05-10 11:58:18 +0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,60 +1,84 @@
 import Container from "@/components/Container";
-import React, {FormEvent, useState} from 'react'
+import { FormEvent, useEffect, useState } from "react";
 
-export default function HomePage(){
+type ContactDefinition = {
+  name: string,
+  phoneNumber: string
+}
+
+export default function HomePage() {
+
   const [nameInput, setNameInput] = useState('');
-  const [phoneInput, setPhoneInput] = useState('');
-  const [contacts, setContacts] = useState([
-    {name: 'Anna Andersson' , phone: '044 1234567'},
-    {name: 'Peter Petersson' , phone: '050 1234567'},
-    {name: 'Johan Jansson' , phone: '040 7654321'}
-  ]);
+  const [phonenumberInput, setPhoneNumberInput] = useState('');
+
+  const [contacts, setContacts] = useState<ContactDefinition[]>([]);
+
+  useEffect(() => {
+    // Load from localstorage and setContacts...
+    setContacts([
+      {name: 'Anna Andersson', phoneNumber: '0501234567'},
+      {name: 'Peter Petersson', phoneNumber: '0501234567'},
+      {name: 'Johan Johansson', phoneNumber: '0501234567'}
+    ]);
+  }, []);
 
   const onAdd = (e: FormEvent) => {
     e.preventDefault();
 
     setContacts([
-      ...contacts, 
-      {name: nameInput, phone: phoneInput}
-      
-    ])
+      ...contacts,
+      {name: nameInput, phoneNumber: phonenumberInput}
+    ]);
+
     setNameInput('');
-    setPhoneInput('');
+    setPhoneNumberInput('');
 
+    // Spara till localstorage
   }
+
+  const onRemove = (index: number) => {
+    const newContacts = [...contacts];
+    newContacts.splice(index, 1);
+    setContacts(newContacts);
+
+    // Spara till localstorage
+  }
+
   return (
-      <Container>
-        <h1>Contacts</h1>
-        <form onSubmit={onAdd}>
-          <label>
-            <span>Name</span>
-            <input type="text" value={nameInput} onChange={(e) => setNameInput(e.target.value)} />
-          </label>
-          <label>
-            <span>Phone number</span>
-            <input type="text" value={phoneInput} onChange={(e) => setPhoneInput(e.target.value)} />
-          </label>
-          <button>Add</button>
-        </form>
+    <Container>
+      <h1>Contacts</h1>
 
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Phone number</th>
-            </tr>
-          </thead>
-          <tbody>
-            {contacts.map(contact => (
-              <tr>
+      <form onSubmit={onAdd}>
+        <label>
+          <span>Name</span>
+          <input type="text" value={nameInput} onChange={e => setNameInput(e.target.value)} />
+        </label>
+        <label>
+          <span>Phonenumber</span>
+          <input type="text" value={phonenumberInput} onChange={e => setPhoneNumberInput(e.target.value)} />
+        </label>
+        <button>Add</button>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phonenumber</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {contacts.map((contact, i) => (
+            <tr key={contact.name + contact.phoneNumber}>
               <td>{contact.name}</td>
-              <td>{contact.phone}</td>
+              <td>{contact.phoneNumber}</td>
+              <td><button onClick={() => onRemove(i)}>X</button></td>
             </tr>
-            ))}
-            
-          </tbody>
-
-        </table>
-      </Container>
-  )
+          ))}
+        </tbody>
+      </table>
+    </Container>
+  );
 }
